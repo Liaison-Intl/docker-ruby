ddc913cffd348573668914db7c3292a4f878f142
pierre.fortincarrier <pierre.fortincarrier@fxinnovation.com>
2016-12-26 10:35:35 -0500

    [nfs-server] favor async over sync lower verbosity after initial initialization

--- alpine/nfs-server/entrypoint.sh
+++ alpine/nfs-server/entrypoint.sh
@@ -5,7 +5,7 @@ set -x
 
 [[ -d /exports/ ]] || mkdir /exports
 touch /exports/bob
-echo "/exports *(rw,fsid=0,no_subtree_check,insecure,no_root_squash)" \
+echo "/exports *(rw,fsid=0,no_subtree_check,insecure,no_root_squash,async)" \
   > /etc/exports
 
 mount -t nfsd nfds /proc/fs/nfsd
@@ -16,6 +16,9 @@ mount -t nfsd nfds /proc/fs/nfsd
   -N 2 -N 3 -V 4 -V 4.1 \
   --no-udp \
   --exports-file /etc/exports --debug all
+
+set +x
+echo "Initialization complete ..."
 
 while true; do
     sleep 5

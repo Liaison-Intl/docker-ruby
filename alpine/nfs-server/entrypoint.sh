0bfb78a5df7d17846f1cd1b1409a45458094b594
pierre.fortincarrier <pierre.fortincarrier@fxinnovation.com>
2016-12-26 10:49:57 -0500

    [nfs-server] add more time related debug output default to /exports being word writable to ease testing purpose

--- alpine/nfs-server/entrypoint.sh
+++ alpine/nfs-server/entrypoint.sh
@@ -3,16 +3,16 @@ set -x
 
 ## No frill entrypoint ...
 
-[[ -d /exports/ ]] || mkdir /exports
+[[ -d /exports/ ]] || ( mkdir /exports ; chmod 0777 /exports )
 touch /exports/bob
 echo "/exports *(rw,fsid=0,no_subtree_check,insecure,no_root_squash,async)" \
   > /etc/exports
 
-mount -t nfsd nfds /proc/fs/nfsd
+time mount -t nfsd nfds /proc/fs/nfsd
 /usr/sbin/rpc.nfsd \
   -N 2 -N 3 -V 4 -V 4.1 --debug 8
-/usr/sbin/exportfs -rv
-/usr/sbin/rpc.mountd \
+time /usr/sbin/exportfs -rv
+time /usr/sbin/rpc.mountd \
   -N 2 -N 3 -V 4 -V 4.1 \
   --no-udp \
   --exports-file /etc/exports --debug all

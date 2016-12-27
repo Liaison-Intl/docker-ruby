08a534347113b0103172fb51a746c74677949732
Pierre Fortin Carrier <pierre.fortincarrier@gmail.com>
2016-12-27 09:26:19 -0500

    [nfs-server] prevent startup hang of rpc.nfsd by starting rpcbind

--- alpine/nfs-server/entrypoint.sh
+++ alpine/nfs-server/entrypoint.sh
@@ -5,19 +5,35 @@ set -x
 
 [[ -d /exports/ ]] || ( mkdir /exports ; chmod 0777 /exports )
 touch /exports/bob
-echo "/exports *(rw,fsid=0,no_subtree_check,insecure,no_root_squash,async)" \
+
+echo "/exports *(rw,fsid=0,no_subtree_check,insecure,no_root_squash,async,no_auth_nlm)" \
   > /etc/exports
 
-time /usr/sbin/rpc.nfsd \
-  -N 2 -N 3 -V 4 -V 4.1 --no-udp -G 10 --debug 8 2
-  # -G 10 to reduce grace time to 10 seconds -- the lowest allowed -- to allow
-  # much quicker startup.  Otherwise can take up to 6 minute ...
-  # trailing '2' indicate 'nrservs'
+mkdir -p \
+  /var/lib/nfs/rpc_pipefs \
+  /var/lib/nfs/v4recovery \
+  /var/lib/nfs/v4root
+
+mount -t nfsd -o nodev,noexec,nosuid nfsd /proc/fs/nfsd
+mount -t rpc_pipefs rpc_pipefs /var/lib/nfs/rpc_pipefs
+
+# While NFSv4 doesn't need portmapper it still try to register to it
+# and will hang for 3 minute on a read to /proc/fs/nfsd/portlist
+# at startup if this daemon is not started
+time rpcbind -w 
+time rpc.idmapd -v
+
 time /usr/sbin/exportfs -rv
+
 time /usr/sbin/rpc.mountd \
   -N 2 -N 3 -V 4 -V 4.1 \
   --no-udp \
   --exports-file /etc/exports --debug all
+
+time strace -s 100 /usr/sbin/rpc.nfsd \
+  -G 10 -N 2 -N 3 -V 4 -V 4.1 --no-udp --debug 8 8
+# ^^ -G 10 to reduce grace time to 10 seconds -- the lowest allowed -- to allow
+#    much quicker startup.  Trailing '8' indicate 'nrservs'
 
 set +x
 echo "Initialization complete ($((${SECONDS}/60)) min $((${SECONDS}%60)) sec)"
@@ -34,6 +50,9 @@ function stop()
   /usr/sbin/exportfs -ua
   /usr/sbin/exportfs -f
   kill $(pidof rpc.mountd)
+  #kill $(pidof rpc.nfsd)
+  umount -f /var/lib/nfs/rpc_pipefs
+  umount -f /proc/fs/nfsd
   echo > /etc/exports
   echo "Terminated."
   exit 0
